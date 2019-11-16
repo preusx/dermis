f90e1c27a2b8b954f9d858061ed68cc46e291434
Alex Tkachenko <preusx.dev@gmail.com>
2019-11-16 19:17:43 +0200

    Added basic docs scaffolding

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  title: 'Dermis',
+  description: 'Set of tools for application styling',
+  plugins: [
+    '@vuepress/back-to-top',
+  ],
+  base: '/',
+  head: [
+    ['meta', { charset: 'utf-8' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    ['meta', { property: 'og:image', content: '/logo.svg' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/img/favicon/ms-icon-144x144.png' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '57x57', href: '/img/favicon/apple-icon-57x57.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '60x60', href: '/img/favicon/apple-icon-60x60.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '72x72', href: '/img/favicon/apple-icon-72x72.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '76x76', href: '/img/favicon/apple-icon-76x76.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '114x114', href: '/img/favicon/apple-icon-114x114.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '120x120', href: '/img/favicon/apple-icon-120x120.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '144x144', href: '/img/favicon/apple-icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '152x152', href: '/img/favicon/apple-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/img/favicon/apple-icon-180x180.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '192x192',  href: '/img/favicon/android-icon-192x192.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/favicon/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/img/favicon/favicon-96x96.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/favicon/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+
+  ],
+  locales: {
+    '/': {
+      lang: 'en-US'
+    },
+    '/ru/': {
+      lang: 'ru',
+      title: 'Dermis',
+      description: 'Набор утилит для стилизации приложения',
+    }
+  },
+  themeConfig: {
+    repo: 'preusx/dermis',
+    docsRepo: 'preusx/dermis',
+    docsDir: 'docs',
+    logo: '/logo.svg',
+    editLinks: true,
+    sidebarDepth: 2,
+    sidebar: 'auto',
+    // sidebar: {
+    //   '/guide/': [
+    //     '',
+    //     'getting-started',
+    //     'basic-validation',
+    //     'applying-rules',
+    //     'displaying-errors',
+    //     'validation-provider',
+    //     'validation-observer',
+    //     'interaction-and-ux',
+    //     'styling',
+    //     'a11y',
+    //     'html5-validation',
+    //     'advanced-validation',
+    //     'localization',
+    //     'testing'
+    //   ],
+    //   '/api/': ['', 'rules'],
+    //   '/examples/': ['ui-libraries', 'i18n', 'multiple-forms', 'backend', 'nuxt']
+    // },
+    locales: {
+      '/': {
+        link: '/',
+        label: 'English',
+        selectText: 'Languages',
+        editLinkText: 'Help us improve this page!',
+        nav: [
+          // { text: 'Guide', link: '/guide/' }
+        ]
+      },
+      '/ru/': {
+        link: '/ru/',
+        label: 'Русский',
+        selectText: 'Языки',
+        editLinkText: 'Помогите нам исправить эту страницу!',
+        nav: [
+          // { text: 'Guide', link: '/guide/' }
+        ]
+      }
+    }
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,20 @@
+---
+lang: en-US
+home: true
+heroImage: /logo.svg
+actionText: Get started »
+actionLink: ./guide/
+features:
+  - title: Simple
+    details: Easy to use and setup. With minimal overwriting involved.
+  - title: Configurable
+    details: Every component in the library exposes a lot of configurations to change.
+  - title: Modular
+    details: Easy to use partially, swap parts. Highly reusable styling.
+footer: MIT Licensed | Copyright © 2019-present Alex Tkachenko
+meta:
+  - name: og:title
+    content: Dermis
+  - name: og:description
+    content: Set of tools for application styling
+---
